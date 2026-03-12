a18350d49a8813cab1b28dadd153bc5d1f0fef62
mdfahimhasan <Fahim.Hasan@colostate.edu>
2026-03-11 23:42:07 -0600

    1st commit

---
+++ Codes/panel_reg/run_panel.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH --partition=smi_all
+#SBATCH --ntasks=2
+#SBATCH --nodes=1
+#SBATCH --time=1-0
+#SBATCH --mail-type=BEGIN,END,FAIL,TIME_LIMIT
+#SBATCH --mail-user=Fahim.Hasan@colostate.edu
+
+python run_panel.py
+
